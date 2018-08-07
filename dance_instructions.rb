41b0ec11c6c931c2965ac0df0f00bac566835c78
chloexiang217 <chloe.xiang217@gmail.com>
2018-08-07 13:32:34 +0000

    automatically backed up by learn

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -1,2 +1,25 @@
+# def dance_instructions
+#   puts "Plant legs far apart, bend knees slightly and keep posture loose"
+#   puts "Lift right foot"
+#   puts "Return right foot to the ground"
+#   puts "Finishing with a small skip-step backward"
+#   puts "Lower left foot to the ground"
+#   puts "Bounce left foot back up slightly, kicking it a few inches back"
+#   puts "Left, Right, Left and Left"
+#   puts "Lower right foot to the ground"
+#   puts "Bounce right foot back up slightly, kicking it a few inches back"
+# end
 
-# Code your methods below
+# dance_instructions
+
+def starting_stance
+  puts "Plant legs far apart, bend knees slightly and keep posture loose"
+end
+
+def base_footwork
+  puts "Lift right foot"
+  puts "Return right foot to the ground"
+  puts "Finishing with a small skip-step backward"
+end 
+
+def skip_step_left
