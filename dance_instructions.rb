74832051b4a83d3992f49f6f9dbbe6dd29dec63c
chloexiang217 <chloe.xiang217@gmail.com>
2018-08-07 13:52:55 +0000

    automatically backed up by learn

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -32,5 +32,7 @@ def skip_step(starting_foot)
    puts "Holding your arms out in front of you, straight and at chest level"
    puts "Cross your right wrist over your left and hold them together"
    puts "Lift your arms up and down in a loose bouncing movement, in time with the beat of the song. This movement is repeated eight times."
+    puts "Do this 7 more times!"
  end
+  
   
