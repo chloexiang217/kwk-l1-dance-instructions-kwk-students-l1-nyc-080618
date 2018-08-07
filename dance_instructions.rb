0831883d2bee7e10ec74dbd613a5a034b5bda853
chloexiang217 <chloe.xiang217@gmail.com>
2018-08-07 13:38:55 +0000

    automatically backed up by learn

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -23,7 +23,7 @@ def base_footwork
 end
 
 def skip_step(starting_foot)
-  puts "Lower left foot to the ground"
+  puts "Lower #{starting_foot} foot to the ground"
   puts "Bounce left foot back up slightly, kicking it a few inches back"
-  puts ""
+  puts "Left, Right, Left and Left"
 end
