1c730ada60f01fcab47a0a352eab9c1099bd18f3
chloexiang217 <chloe.xiang217@gmail.com>
2018-08-07 13:53:21 +0000

    automatically backed up by learn

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -36,4 +36,5 @@ def skip_step(starting_foot)
  end
   
   def lasso
+    puts "Start arm movement by holding left arm so the back of your wrist is near your chin, pointing your left elbow straight left."
     puts ""
