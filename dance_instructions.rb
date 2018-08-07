de01d3320f313e94b7afd1c0d563332749546db0
chloexiang217 <chloe.xiang217@gmail.com>
2018-08-07 13:33:14 +0000

    automatically backed up by learn

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -12,14 +12,3 @@
 
 # dance_instructions
 
-def starting_stance
-  puts "Plant legs far apart, bend knees slightly and keep posture loose"
-end
-
-def base_footwork
-  puts "Lift right foot"
-  puts "Return right foot to the ground"
-  puts "Finishing with a small skip-step backward"
-end 
-
-def skip_step_left
